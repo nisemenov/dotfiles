ebcc273bd64d67a7eda1bd5d2ac49919eb60a684
Nikita Semenov <nasemenov726@gmail.com>
2025-07-04 07:47:31 +0300

    add docker

---
+++ docker/docker_init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e  # остановка при ошибке
+
+cd ~/Dev || exit 1
+
+docker compose -f docker-compose.yml down
+docker compose -f docker-compose.yml build
+docker compose -f docker-compose.yml up -d
